887cb7d9fa22cdc33025f2797fe7e4c0f689bab3
Carles Climent <carlescliment@hotmail.com>
2013-10-23 18:23:45 +0200

    Switch to shuber curl library

--- carlescliment/Html2PdfServiceBundle/Bridge/CurlProtocol.php
+++ carlescliment/Html2PdfServiceBundle/Bridge/CurlProtocol.php
@@ -3,47 +3,30 @@
 namespace carlescliment\Html2PdfServiceBundle\Bridge;
 
 
+use shuber\Curl\Curl;
 
-/**
- * set_time_limit(0);
- *
- * $url = 'http://www.freewarelovers.com/android/download/temp/1306495040_Number_Blink_1.1.1.apk';
- * $fp = fopen (dirname(__FILE__) . '/downloads/a.apk', 'w+');
- *
- *     $ch = curl_init($url);
- *
- *     curl_setopt_array($ch, array(
- *     CURLOPT_URL            => $url,
- *     CURLOPT_BINARYTRANSFER => 1,
- *     CURLOPT_RETURNTRANSFER => 1,
- *     CURLOPT_FILE           => $fp,
- *     CURLOPT_TIMEOUT        => 50,
- *     CURLOPT_USERAGENT      => 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)'
- *     ));
- *
- * $results = curl_exec($ch);
- * if(curl_exec($ch) === false)
- *  {
- *   echo 'Curl error: ' . curl_error($ch);
- *  }
- */
+
 class CurlProtocol extends Protocol
 {
 
-    public function create($html, $resource_name)
+    private $curl;
+
+    public function __construct(Curl $curl)
     {
-        $this->requestResourceGeneration();
-        // ...
+        $this->curl = $curl;
     }
 
-    private function requestResourceGeneration($resource_name)
+    public function create($html, $resource_name)
     {
-        $channel = \curl_init($this->host . '/' . $resource_name);
-        $temporary_file = fopen('php://temp/maxmemory:16000', 'w');
-        \curl_setopt($channel, \CURLOPT_BINARYTRANSFER, 1);
-        \curl_setopt($channel, \CURLOPT_RETURNTRANSFER, 1);
-        \curl_setopt($channel, \CURLOPT_PUT, 1);
-        \curl_setopt($channel, \CURLOPT_FILE, $temporary_file);
-        return curl_exec($ch);
+        $response = $this->requestResourceGeneration($html, $resource_name);
+    }
+
+    private function requestResourceGeneration($html, $resource_name)
+    {
+        $parameters = array(
+            'content' => $html
+            );
+        $url = $this->host . '/' . $resource_name;
+        return $this->curl->put($url, $parameters);
     }
 }
---
+++ carlescliment/Html2PdfServiceBundle/Tests/Bridge/CurlProtocolTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace carlescliment\Html2PdfServiceBundle\Tests\Bridge;
+
+use carlescliment\Html2PdfServiceBundle\Bridge\CurlProtocol;
+
+
+class CurlProtocolTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $curl;
+    private $protocol;
+
+    public function setUp()
+    {
+        $this->curl = $this->getMock('shuber\Curl\Curl');
+        $this->protocol = new CurlProtocol($this->curl);
+    }
+
+
+    /**
+     * @test
+     */
+    public function itCreatesTheRemoteDocument()
+    {
+        $content = '<html></html>';
+        $this->protocol->setHost('http://remote.pdf.com');
+        $expected_arguments = array('content' => $content);
+        $expected_url = 'http://remote.pdf.com/resource_name';
+
+        $this->curl->expects($this->once())
+            ->method('put')
+            ->with($expected_url, $expected_arguments);
+
+        $this->protocol->create($content, 'resource_name');
+    }
+
+}
